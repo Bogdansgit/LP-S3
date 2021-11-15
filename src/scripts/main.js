17283b88bdd9c2c8a41be45c544e4c0b08dde30b
Bogdan <bogdanemails@gmail.com>
2021-11-15 22:59:27 +0200

    opinions block was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const list = document.querySelector('.opinions__opinion-box');
+const prev = document.querySelector('.opinions__slider-button--prev');
+const next = document.querySelector('.opinions__slider-button--next');
+
+let position = 0;
+
+function move(shift) {
+  const count = list.children.length;
+
+  position += shift;
+  position = Math.abs(position) % count;
+
+  list.style.transform = `translateX(${-position * 100}%)`;
+}
+
+next.addEventListener('click', function() {
+  move(1);
+});
+
+prev.addEventListener('click', function() {
+  move(-1);
+});
